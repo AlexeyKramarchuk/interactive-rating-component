6b48d25e6521ff1c6210e4f3efdec9db01cab9d5
Oleksii-Kramar <dp020991kos@gmail.com>
2022-11-27 22:36:22 +0100

    active_class_js

--- script/script.js
+++ script/script.js
@@ -1,43 +1,34 @@
-const nums = document.querySelectorAll('.num');
-const numsParent = document.querySelector('.choice')
+const wrapper = document.querySelector('.choice')
 const btn = document.querySelector('button');
 
-nums.forEach(item => {
-    item.addEventListener('click', () => {
-        if (!item.classList.contains('active')) {
-            item.classList.add('active');
-        } else {
-            item.classList.remove('active');
-        }
-    })
-})
-
-// function addActive() {
-//     nums.forEach(item => {
-//         item.classList.add('active')
-//     })
-// }
-
-// function removeActive() {
-//     nums.forEach(item => {
-//         item.classList.remove('active')
-//     })
-// }
-
-// nums.addEventListener('click', (event) => {
-
-//     if (item.classList.contains('active')) {
-//         nums.forEach((item, i) => {
-//             if (target === item) {
-//                 removeActive();
-//                 addActive(i);
-//             }
-//         });
+// (function(){
+//     wrapper.addEventListener('click', function(e) {
+//     let buttons = document.querySelectorAll('.num');
+//     let target = e.target;
+//     if(!target.classList.contains('wrapper')) {
+//     	  [].forEach.call(buttons, function(elem){ 
+//      		 elem.classList.remove('active');
+//    		  });
+//    		   target.classList.add('active');
 //     }
-// })
+// 	})
+// })();
 
+
+(function(){
+    wrapper.addEventListener('click', function(e) {
+    let buttons = document.querySelectorAll('.num');
+    let target = e.target;
+
+    Object.values(buttons).forEach((btn) => {
+      btn.classList.remove('active');
+    });
+
+    target.classList.add('active');
+ })
+})();
 
 
 btn.addEventListener('click', () => {
-    console.log("click")
+    window.location.href = 'thank.html';
 })
